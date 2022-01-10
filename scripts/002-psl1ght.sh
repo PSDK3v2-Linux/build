64d99a1e8495e1902a4d92cd0dfeeca2d93dce63
rhynec <rhynec@pm.me>
2022-01-10 11:42:31 -0700

    Bump PSL1GHT version
    - Newer PSL1GHT builds ps3py correctly.

--- scripts/002-psl1ght.sh
+++ scripts/002-psl1ght.sh
@@ -2,7 +2,7 @@
 # psl1ght.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/rhynec/PSL1GHT/archive/f1f2e0fda566e91a8653f6b7da03786e97cb5ef4.tar.gz -O psl1ght.tar.gz
+wget --no-check-certificate https://github.com/rhynec/PSL1GHT/archive/8af60451d71a43e3473f21f5e334b8b74dc8d8f8.tar.gz -O psl1ght.tar.gz
 
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
