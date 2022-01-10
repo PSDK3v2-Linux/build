97a4d5967f15c7367ec8cbbac4e82534f23d4cd5
rhynec <rhynec@pm.me>
2022-01-10 15:04:02 -0700

    Bump ps3libraries version
    - Fix broken makefile for ps3soundlib.

--- scripts/003-ps3libraries.sh
+++ scripts/003-ps3libraries.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/rhynec/ps3libraries/archive/7b98ba636c63c08815227b5c24adcbe9c89283e5.tar.gz -O ps3libraries.tar.gz
+wget --no-check-certificate https://github.com/rhynec/ps3libraries/archive/81c9b1c72d95d2820353a4a2916af3020f8455dd.tar.gz -O ps3libraries.tar.gz
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
