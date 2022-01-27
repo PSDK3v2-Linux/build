077b037cbd5cf184a09598b71a9fd37c3e4ac85b
rhynec <rhynec@pm.me>
2022-01-26 22:09:00 -0600

    Fix tarball links
    - PSL1GHT/toolchain/libraries for PSDK3v2 moved to an organization in GitHub.

--- scripts/001-toolchain.sh
+++ scripts/001-toolchain.sh
@@ -2,7 +2,7 @@
 # ps3toolchain.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/rhynec/ps3toolchain/archive/16fc338f42525c256b92316d5610ca5f584994aa.tar.gz -O ps3toolchain.tar.gz
+wget --no-check-certificate https://github.com/PSDK3v2-Linux/toolchain/archive/16fc338f42525c256b92316d5610ca5f584994aa.tar.gz -O ps3toolchain.tar.gz
 
 ## Unpack the source code.
 rm -Rf ps3toolchain && mkdir ps3toolchain && tar --strip-components=1 --directory=ps3toolchain -xvzf ps3toolchain.tar.gz && cd ps3toolchain
--- scripts/003-ps3libraries.sh
+++ scripts/003-ps3libraries.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/rhynec/ps3libraries/archive/20dfb28187495b673e6a742737d452e829d3c6ee.tar.gz -O ps3libraries.tar.gz
+wget --no-check-certificate https://github.com/PSDK3v2-Linux/libraries/archive/9a99d7272530bdb23a1135ad0fe94811236c4868.tar.gz -O ps3libraries.tar.gz
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
