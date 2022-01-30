d5d2dea50890cefabca5f36bdae3d2d5fe6d6f04
rhynec <rhynec@pm.me>
2022-01-30 17:23:49 -0600

    Bump PSL1GHT version
    - Modified scetool now completely quiet during default pkg builds.

--- scripts/002-psl1ght.sh
+++ scripts/002-psl1ght.sh
@@ -2,7 +2,7 @@
 # psl1ght.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/PSDK3v2-Linux/PSL1GHT/archive/b04b928e02ca944daca290cca8c8ff660c50655e.tar.gz -O psl1ght.tar.gz
+wget --no-check-certificate https://github.com/PSDK3v2-Linux/PSL1GHT/archive/21697c8f71c398250e75d72522ecc8679c9d1ff9.tar.gz -O psl1ght.tar.gz
 
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
